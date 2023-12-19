b9721646bc5c86db79d374137476f01de395bce0
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-12-19 22:43:57 +0545

    feat: sections update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Features from "./sections/Features"
 import Footer from "./sections/Footer"
+import Plugins from "./sections/Plugins"
+import Sounds from "./sections/Sounds"
+import Studio from "./sections/Studio/Studio"
 import Testimonial from "./sections/Testimonial"
 
 function App() {
 
   return (
     <div className='app font-mont'>
+      <Sounds />
+      <Plugins />
+      <Studio />
       <Testimonial />
       <Features />
       <Footer />
