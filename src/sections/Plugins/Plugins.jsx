31dc78e751fb0e5e32cfa5434137b39955ad3ce8
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-12-19 23:20:07 +0545

    refactor: Sounds and plugins background update

--- src/sections/Plugins/Plugins.jsx
+++ src/sections/Plugins/Plugins.jsx
@@ -2,10 +2,12 @@ import CTA from "../../elements/CTA";
 import H1 from "../../elements/H1";
 import H3 from "../../elements/H3";
 import PluginImg from '../../assets/plugins.png';
+import CurveNegativeSvg from '../../assets/curveNegative.svg';
 
 export default function Plugins() {
     return (
-        <div className="px-8 py-24 bg-gradient-to-b from-[#F4F6F9] via-[#FAF6F9] to-white">
+        <div className="px-8 py-24 bg-gradient-to-b from-[#F4F6F9] via-[#FAF6F9] to-white relative">
+            <img src={CurveNegativeSvg} alt="" className="absolute top-0 left-0 rotate-180 w-full" />
             <div className="mx-auto flex justify-between items-center max-w-[1280px] flex-col-reverse gap-12 md:flex-row">
                 <div>
                     <img src={PluginImg} alt="plugins" />
--- src/sections/Sounds/Sounds.jsx
+++ src/sections/Sounds/Sounds.jsx
@@ -2,12 +2,13 @@ import CTA from "../../elements/CTA";
 import H1 from "../../elements/H1";
 import H3 from "../../elements/H3";
 import SoundImg from '../../assets/sounds.png'
+import CurveylineSvg from '../../assets/curveyline.svg'
 
 export default function Sounds() {
     return (
-        <div className="px-8 py-12">
+        <div className="px-8 py-12 relative">
             <div className="mx-auto max-w-[640px] flex flex-col text-center gap-12">
-                <div>
+                <div className="z-10">
                     <H3 className='text-[#605f6080]'>Sounds</H3>
 
                     <H1 className='text-[#272727]'>
@@ -24,8 +25,10 @@ export default function Sounds() {
                     </div>
                 </div>
 
-                <img src={SoundImg} alt="" />
+                <img src={SoundImg} alt="" className="z-10" />
             </div>
+
+            <img src={CurveylineSvg} alt="" className="absolute top-44 left-0 w-full" />
         </div>
     )
 }
